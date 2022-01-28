f9c067a5816a92f730164044a7b131fcbc64ae4c
josecossi89 <josecossib@gmail.com>
2022-01-28 11:44:12 -0300

    styles and backgrounds add

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+const api = {
+  key: "f9f1e7370232ef4f9c5f9a94cfe035ff",
+  base: "https://openweathermap.org/data/2.5/",
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app ">
+      <main>
+        <div class="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
     </div>
   );
 }

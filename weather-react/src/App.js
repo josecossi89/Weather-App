3f78a52c071da67f90218ef8df9a574ba95ec24a
josecossi89 <josecossib@gmail.com>
2022-01-29 11:11:32 -0300

    deploy ready

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -66,7 +66,7 @@ function App() {
           <input
             type="text"
             className="search-bar"
-            placeholder="Search Countrie..."
+            placeholder="Search..."
             onChange={(e) => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
@@ -82,7 +82,7 @@ function App() {
             </div>
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°c</div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="weather">{weather.weather[0].description}</div>
             </div>
           </div>
         ) : (
